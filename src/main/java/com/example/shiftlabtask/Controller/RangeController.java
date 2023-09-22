c5b0e0a0f199edb08ded15ccee4cdaeb7052090f
Aleksey Shumkov <shumleshas@gmail.com>
2023-09-22 11:55:54 +0700

    Added some necessary checkers for kind of interval bounds

--- src/main/java/com/example/shiftlabtask/Controller/RangeController.java
+++ src/main/java/com/example/shiftlabtask/Controller/RangeController.java
@@ -4,6 +4,7 @@ import com.example.shiftlabtask.CustomInterval;
 import com.example.shiftlabtask.IntervalProcessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,13 +23,20 @@ public class RangeController {
 
 
     @PostMapping("/merge")
-    public void Merge(@RequestBody List<CustomInterval> intervals, @RequestParam("kind") String kind){
+    public ResponseEntity<String> Merge(@RequestBody List<CustomInterval> intervals, @RequestParam("kind") String kind){
+        if (!"letters".equals(kind) && !"digits".equals(kind)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("'kind' parametr can be equal only letters/digits");
+        }
         intervals.forEach(interval -> interval.setIntervalKind(kind));
         IntervalProcessor.processIntevals(intervals);
+        return ResponseEntity.ok("Intervals were merged and added to h2 DB");
     }
 
     @GetMapping("/min")
     public ResponseEntity<String> getMinimalInterval( @RequestParam("kind") String kind) throws JsonProcessingException {
+        if (!"letters".equals(kind) && !"digits".equals(kind)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("'kind' parametr can be equal only letters/digits");
+        }
         CustomInterval minimal = IntervalProcessor.getmin(kind);
         String json = objectMapper.writeValueAsString(minimal);
         return ResponseEntity.ok(json);
